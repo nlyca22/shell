65ced67ce66a1c3788668946adb3dbd572e7843b
Ly Nguyen <nly.ca22@gmail.com>
2024-08-22 14:26:48 +0700

    I have made changes to assignment.sh file

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -16,28 +16,25 @@ ls
 
 
 # 3. Create 5 text files in dir2 named file1, file2, file3, file4, and file5
-cd dir2
-touch file1 file2 file3 file4 file5
-
+cd dir2 && touch file1.txt file2.txt file3.txt file4.txt file5.txt
+#personal note: need to add ".txt" as the format is "txt file", and use "&&" to combine 2 command lines into 1 line
 
 # 4. Append the words "Hello world" to dir2/file3
-echo "Hello world" >> file3
-
-
+echo "Hello world" >> dir2/file3.txt
+#personal note: ">" overwrites all content in the file, ">>" only appends. Added ".txt" as we specifically created text files.
 
 # 5. Verify that file3 contains the words "hello world" by printing the contents of the file in the terminal
-cat file3
-
+cat dir2/file3.txt
+#personal note: need to clarify which folder we're in as we're still in the parent directory, and remember ".txt" file format
 
 # 6. Delete file4
-rm file4
-
+rm dir2/file4.txt
+#personal note: need to clarify which folder we're in as we're still in the parent directory, and remember ".txt" file format
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
 rm -r dir4 dir5
-
+#personal note: "-r" means recursive, it will delete all files and directories there (not just files)
 
 # 8. List the contents of the parent directory to verify the deletion of dir4 and dir5
-cd ../
 ls
-
+#personal note: we're still in parent directory
